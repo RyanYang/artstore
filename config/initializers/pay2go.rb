8f881270f5ed58f149d2b4c238c5a20f8d797888
Ryan Yang <aeolus0517@gmail.com>
2016-03-27 14:29:47 +0800

    Add figaro gem and set payment keys to figaro

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,15 +1,9 @@
 Pay2go.setup do |pay2go|
-  if Rails.env.development?
-    pay2go.merchant_id = "11656180"  # 放測試站的 key
 
-    pay2go.hash_key    = "6QN17rhlVSQ9QRORlB39mKKSsOufQApW"
-    pay2go.hash_iv     = "qcXxEfTA7JJYAyQW"
-    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
-  else
-    pay2go.merchant_id = "1234567"  # 放正式站的 key
+  # 已經在 figaro 做完判斷 development or production
+  pay2go.merchant_id = ENV['pay2go_merchant_id']  # 放測試站的 key
+  pay2go.hash_key    = ENV['pay2go_hash_key']
+  pay2go.hash_iv     = ENV['pay2go_hash_iv']
+  pay2go.service_url = ENV['pay2go_service_url']
 
-    pay2go.hash_key    = "xxxxxxxx"
-    pay2go.hash_iv     = "xxxxxxxx"
-    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
-  end
 end
